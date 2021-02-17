54f082c6f1e6f33967ff92ad5f12c7e28a93004f
krayzieeddi <63763467+krayzieeddi@users.noreply.github.com>
2021-02-16 18:03:37 -0800

    big extension to site

---
+++ big/includes/footer.php
@@ -0,0 +1,21 @@
+<!-- START Footer ///////////////////////////////////// -->
+<footer>
+    
+<!-- validator for html and css in this block -->
+    <ul>
+        <li>&copy; 2018 - <?=date("Y")?></li>
+        <li><a href="../contactme.php" target="_blank">Edwin Ho</a></li>
+        <li>All Rights Reserved</li>
+        <li><a href="http://validator.w3.org/check/referer" target="_blank">Valid HTML</a></li>
+        <li><a href="http://jigsaw.w3.org/css-validator/check?uri=referer" target="_blank">Valid CSS</a></li>
+    </ul>
+    
+</footer>
+<!-- END Footer ////////////////////////////// --> 
+    
+</main> 
+<!-- END of main wrapper section /////////////////////////////////////// -->
+
+
+</body>
+</html>
---
+++ big/index.php
@@ -0,0 +1,23 @@
+<?php include "includes/header.php"?>
+<!-- START LEFT COL ////////////////////////////// -->
+<section>
+    
+ <h2 class="pageID">Welcome</h2>
+    
+ <p><strong>Hi, my name is Edwin Ho</strong> and I'm a second year student in the AA computer science program at seattle central community college. I was born and raised in Seattle.</p>
+    
+ <form>add multiple.php client form here</form>
+
+</section>
+<!-- END LEFT COL //////////////////////////// -->
+
+<!-- START RIGHT COL /////////////////////////////////// -->
+<aside >
+    
+ <h3>basic web design cycle</h3>
+
+ <p>Provide 3 resources that help you with stuff</p>
+
+</aside>
+<!-- END RIGHT COL ///////////////////////////////////// -->
+<?php include "includes/footer.php"?>
