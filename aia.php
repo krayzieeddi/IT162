372f434ecc2e1c3c6e44e974abdc3b2c536d908f
krayzieeddi <63763467+krayzieeddi@users.noreply.github.com>
2021-03-03 17:38:59 -0800

    Add files via upload

--- aia.php
+++ aia.php
@@ -1,5 +1,104 @@
 <?php include "includes/header.php";?>
 
-<h2 class="pageID">This is the AIA page.</h2>
+<h2 class="pageID">Edwin Ho's AIA page.</h2>
+
+<!-- START of the client's table information /////////////////////////////// -->
+    <div class="table1">
+        <div class="grid-item"><b>Client's name and the final decision maker</b></div>
+        <div class="grid-item">Sandra Lopez, my girlfriend.</div>
+        <div class="grid-item"><b>client's business/website name</b></div>
+        <div class="grid-item">Sandra Lopez, MAEd School Counseling</div>
+        <div class="grid-item"><b>redesign or a brand new website?</b></div>
+        <div class="grid-item">Brand new website</div>
+        <div class="grid-item"><b>type of website you're producing</b>
+        </div>
+        <div class="grid-item">Marketing and educational</div>
+        <div class="grid-item"><b>client's goal(s)</b></div>
+        <div class="grid-item">An online web page to present herself professionally as a school counselor. 
+            A page where parents and educators can visit to see her credentials. </div>
+    </div>
+<!-- END of client's table information  -->
+
+<!-- START of table 2 information /////////////////////////////////////////////// -->
+    <div class="table2">
+        <div class="grid-item"><h2>Audience</h2></div>
+        <div class="grid-item"><h2>Issues, Concerns</h2></div>
+        <div class="grid-item"><h2>Needs</h2></div>
+
+        <!-- START of parent row //////////////////// -->
+        <div class="grid-item"><b>Parents - </b>for the parents of the children that Sandra would be 
+            counseling, would want to know who is talking to their kids. Concerned parents would want 
+            to know her background and education. Also to contact her in case of any further concern 
+            for their child’s mental and educational well-being. </div>
+
+        <div class="grid-item">
+            <h3>Needs</h3>
+            <ul>
+                <li class="grid-list-item">Wants to see her credentials</li>
+                <li class="grid-list-item">contact information</li>
+                <li class="grid-list-item">displaying personal disclosure statment</li>
+            </ul>
+        </div>
+
+        <div class="grid-item">
+            <h3>Approach</h3>
+            <ul>
+                <li class="grid-list-item">Separate page for legal confidentiality for Sandra</li>
+                <li class="grid-list-item">Contact info in the footer</li>
+                <li class="grid-list-item">Sections for education and certificate on the main page</li>
+            </ul>
+        </div>
+        <!-- END of parent row -->
+        <!-- START of supervisor row /////////////////////////////// -->
+        <div class="grid-item"><b>Supervisors - </b>are another audience group who needs to see her 
+            credentials and background for licensing purposes. Supervisors will supervise Sandra’s 
+            works in the field making sure her work with her clients is up to par and guiding her 
+            through the process of being a fully licensed professional.  </div>
+
+        <div class="grid-item">
+            <h3>Needs</h3>
+            <ul>
+                <li class="grid-list-item">Wants to see her credentials</li>
+                <li class="grid-list-item">Contact information</li>
+                <li class="grid-list-item">Current work site and projects</li>
+            </ul>
+        </div>
+
+        <div class="grid-item">
+            <h3>Approach</h3>
+            <ul>
+                <li class="grid-list-item">A page for current work and projects</li>
+                <li class="grid-list-item">Contact info in the footer</li>
+                <li class="grid-list-item">Sections for education and certificate on the main page</li>
+            </ul>
+        </div>
+        <!-- END of supervisor row -->
+        <!-- START of educator row /////////////////////////////// -->
+        <div class="grid-item"><b>Educators - </b>are school staff from the various districts who would 
+            want to know about her past and current work. Those who work in her current school district, 
+            the educators would want to reach out for possible employment at their current schools if the 
+            needs call for it. Which then they would consider her for employment in any capacity for their 
+            schools.</div>
+
+        <div class="grid-item">
+            <h3>Needs</h3>
+            <ul>
+                <li class="grid-list-item">Wants to see her credentials</li>
+                <li class="grid-list-item">Contact information</li>
+                <li class="grid-list-item">Current work site and projects</li>
+            </ul>
+        </div>
+
+        <div class="grid-item">
+            <h3>Approach</h3>
+            <ul>
+                <li class="grid-list-item">A page for current work and projects</li>
+                <li class="grid-list-item">Contact info in the footer</li>
+                <li class="grid-list-item">Sections for education and certificate on the main page</li>
+            </ul>
+        </div>
+        <!-- END of educator row -->
+    </div>
+<!-- END of table 2 information  -->
 
 <?php include "includes/footer.php";?>
